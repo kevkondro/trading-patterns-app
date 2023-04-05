22369a9478b28914a1cc703a50d7288b77003f4d
kevkondro <77845927+kevkondro@users.noreply.github.com>
2023-04-05 12:26:44 +0200

    x

--- src/App.js
+++ src/App.js
@@ -4,23 +4,38 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [message, setMessage] = useState("");
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/message")
+    fetch("http://localhost:8000/data")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);
 
   useEffect(() => {
+    fetch("http://localhost:8000/data")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  useEffect(() => {
     document.title = "Trading Patterns";
   }, []);
+  
   return (
     <div>
       <h1 style={{ fontFamily: "Droid Sans" }}>Trading Patterns</h1>
       <Tabs />
-      <h1>{message}</h1>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index}>
+              <td>{row[0]}</td>
+              <td>{row[4]}</td>
+            </tr>
+          ))}
+        </tbody>
+
     </div>
-    
   );
 }
 
--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,52 @@
 const express = require("express");
 const cors = require("cors");
+const { google } = require('googleapis');
 const app = express();
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/message", (req, res) => {
-  res.json({ message: "Hello from server!" });
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+/**
+ * Authorizes a client using service account credentials.
+ *
+ * @return {Promise<google.auth.GoogleAuth>}
+ */
+async function authorize() {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: CREDENTIALS_PATH,
+    scopes: SCOPES,
+  });
+
+  const client = await auth.getClient();
+
+  return client;
+}
+
+/**
+ * Retrieves data from a Google Sheets spreadsheet and sends it as a JSON response.
+ *
+ * @param {express.Request} req The HTTP request object.
+ * @param {express.Response} res The HTTP response object.
+ */
+app.get("/data", async (req, res) => {
+  try {
+    const client = await authorize();
+    const sheets = google.sheets({ version: 'v4', auth: client });
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: '1wzzOgKLx3ox6HPBSQf62Ukf63KG8MXtgwWgpm_4dltk',
+      range: 'Result estimation!A2:E',
+    });
+    const data = response.data.values;
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.listen(8000, () => {
