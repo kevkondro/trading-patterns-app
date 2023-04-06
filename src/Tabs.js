02e3e66317a46c69de2864cefd03bd8b402ee5b4
kevkondro <77845927+kevkondro@users.noreply.github.com>
2023-04-06 08:40:00 +0200

    Update Tabs.js

--- src/Tabs.js
+++ src/Tabs.js
@@ -6,7 +6,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const Tab1 = lazy(() => import("./ChartSearch.js"));
 const Tab2 = lazy(() => import("./Rules.js"));
 const Tab3 = lazy(() => import("./Probabilities.js"));
-const Tab4 = lazy(() => import("./Notes.js"));
+// const Tab4 = lazy(() => import("./Notes.js"));
 const Tab5 = lazy(() => import("./DataDisplay.js"));
 const useStyles = makeStyles({
   tabs: {
@@ -39,16 +39,16 @@ export default function Tabs() {
         <MuiTab label="Search" className={classes.tabs}/>
         <MuiTab label="Rules" className={classes.tabs}/>
         <MuiTab label="Probs" className={classes.tabs}/>
-        <MuiTab label="Notes" className={classes.tabs}/>
         <MuiTab label="Data" className={classes.tabs}/>
+        {/* <MuiTab label="Data" className={classes.tabs}/> */}
       </MuiTabs>
       <div>
         <Suspense fallback={<div>Loading...</div>}>
           {activeTab === 0 && <Tab1 />}
           {activeTab === 1 && <Tab2 />}
           {activeTab === 2 && <Tab3 />}
-          {activeTab === 3 && <Tab4 />}
-          {activeTab === 4 && <Tab5 />}
+          {activeTab === 3 && <Tab5 />}
+          {/* {activeTab === 4 && <Tab5 />} */}
         </Suspense>
       </div>
     </div>
